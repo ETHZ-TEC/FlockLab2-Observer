59f26ebc6f0f8be7b300bfdbfb1e7cd27f0b0363
rdaforno <reto.daforno@tik.ee.ethz.ch>
2020-02-07 16:05:51 +0100

    target control script updated

--- testmanagement/tg_ctrl.py
+++ testmanagement/tg_ctrl.py
@@ -32,17 +32,14 @@ class Error(Exception):
 def usage():
     print("Usage: %s --action=<action> [--target=<int>] [--vcc=<float>] [--help]" %sys.argv[0])
     print("Options:")
-    print("  --action, -a\tpossible values:")
-    print("\t\t   enable\tenable the target power")
-    print("\t\t   disable\tdisable the target power")
-    print("\t\t   state\tpoll the current power state")
-    print("\t\t   select\tselect the target specified with --target")
-    print("\t\t   active\tget the currently selected target adapter")
-    print("\t\t   volt\t\tset the target voltage")
-    print("\t\t   reset\treset the target")
-    print("  --vcc, -v\tOptional. Specify the target voltage for action 'volt'.")
-    print("  --target, -t\tOptional. If set, the requested target's interface is enabled. If no target is given, all target's interfaces are turned off.")
-    print("  --help, -h\tOptional. Print this help.")
+    print("  --enable, -e\t\tenable the target power")
+    print("  --disable, -d\t\tdisable the target power")
+    print("  --power, -p\t\tpoll the current power state")
+    print("  --select, -s\t\tselect the target slot (1 - 4)")
+    print("  --active, -a\t\tget the currently selected target adapter")
+    print("  --voltage, -v\t\tset the target voltage (1.1 - 3.6)")
+    print("  --reset, -r\t\treset the target")
+    print("  --help, -h\t\tOptional. Print this help.")
 ### END usage()
 
 
@@ -60,69 +57,46 @@ def main(argv):
     # Get command line parameters
     try:
         # Note: a ':' indicates that the option requires an argument
-        opts, args = getopt.getopt(argv, "ha:t:v:", ["help", "action", "target", "vcc"])
+        opts, args = getopt.getopt(argv, "hedprs:av:", ["help", "enable", "disable", "power", "reset", "select", "active", "voltage"])
     except getopt.GetoptError as err:
         print(str(err))
         sys.exit(errno.EINVAL)
     
-    # Parse arguments
+    # Parse arguments and execute commands
     for opt, arg in opts:
-        if opt in ("-a", "--action"):
-            action = arg
-        elif opt in ("-t", "--target"):
+        if opt in ("-e", "--enable"):
+            flocklab.tg_pwr_en(True)
+            flocklab.tg_en(True)
+        elif opt in ("-d", "--disable"):
+            flocklab.tg_pwr_en(False)
+            flocklab.tg_en(False)
+        elif opt in ("-p", "--power"):
+            if flocklab.tg_pwr_state() > 0:
+                print("power state: ON")
+            else:
+                print("power state: OFF")
+        elif opt in ("-r", "--reset"):
+            flocklab.tg_reset()
+        elif opt in ("-a", "--active"):
+            print("active target slot: %d" % flocklab.tg_get_selected())
+        elif opt in ("-s", "--select", "--sel"):
             try:
                 target = int(arg)
                 if ((target < 1) or (target > 4)):
                     raise ValueError
+                flocklab.tg_select(target)
             except:
-                print("Invalid target")
-                sys.exit(errno.EINVAL)
-        elif opt in ("-v", "--vcc"):
+                print("Invalid target slot")
+        elif opt in ("-v", "--vcc", "--voltage"):
             try:
                 vcc = float(arg)
+                if flocklab.tg_set_vcc(vcc) != flocklab.SUCCESS:
+                    print("Failed to set target voltage.")
             except:
                 print("Invalid voltage")
-                sys.exit(errno.EINVAL)
         elif opt in ("-h", "--help"):
             usage()
             sys.exit(flocklab.SUCCESS)
-    
-    # Check supplied arguments
-    if action is None:
-        print("No action specified")
-        sys.exit(errno.EINVAL)
-
-    if action in "enable":
-        flocklab.tg_pwr_en(True)
-        flocklab.tg_en(True)
-    elif action in "disable":
-        flocklab.tg_pwr_en(False)
-        flocklab.tg_en(False)
-    elif action in "state":
-        if flocklab.tg_pwr_state():
-            print("on")
-        else:
-            print("off")
-    elif action in "active":
-        print(flocklab.tg_get_selected())
-    elif action in ("select", "sel"):
-        if target is None:
-            print("No target specified")
-            sys.exit(errno.EINVAL)
-        flocklab.tg_select(target)
-    elif action in ("volt", "voltage"):
-        if flocklab.tg_set_vcc(vcc) != flocklab.SUCCESS:
-            print("Failed to set target voltage.")
-            sys.exit(errno.EINVAL)
-    elif action in "reset":
-        flocklab.tg_reset()
-    else:
-        print("unknown action '%s'" % action)
-        sys.exit(errno.EINVAL)
-    
-    sys.exit(flocklab.SUCCESS)
-    
-    
 ### END main()
 
 if __name__ == "__main__":
