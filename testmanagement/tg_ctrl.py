a53740d0dd5cd702aeac1f03e59b9b3498d5b773
rdaforno <reto.daforno@tik.ee.ethz.ch>
2020-02-20 14:04:28 +0100

    bugfix for long arguments and new parameters added

--- testmanagement/tg_ctrl.py
+++ testmanagement/tg_ctrl.py
@@ -36,8 +36,10 @@ def usage():
     print("  --disable, -d\t\tdisable the target power")
     print("  --power, -p\t\tpoll the current power state")
     print("  --select, -s\t\tselect the target slot (1 - 4)")
-    print("  --active, -a\t\tget the currently selected target adapter")
+    print("  --target, -t\t\tget the currently selected (active) target slot")
     print("  --voltage, -v\t\tset the target voltage (1.1 - 3.6)")
+    print("  --mux, -m\t\tenable or disable the MUX")
+    print("  --actuation, -a\tenable or disable actuation")
     print("  --reset, -r\t\treset the target")
     print("  --help, -h\t\tOptional. Print this help.")
 ### END usage()
@@ -49,15 +51,11 @@ def usage():
 #
 ##############################################################################
 def main(argv):
-
-    action = None
-    target = None
-    vcc    = 3.0
     
     # Get command line parameters
     try:
         # Note: a ':' indicates that the option requires an argument
-        opts, args = getopt.getopt(argv, "hedprs:av:", ["help", "enable", "disable", "power", "reset", "select", "active", "voltage"])
+        opts, args = getopt.getopt(argv, "hedprs:m:a:tv:", ["help", "enable", "disable", "power", "reset", "select=", "mux=", "actuation=", "target", "voltage="])
     except getopt.GetoptError as err:
         print(str(err))
         sys.exit(errno.EINVAL)
@@ -77,7 +75,7 @@ def main(argv):
                 print("power state: OFF")
         elif opt in ("-r", "--reset"):
             flocklab.tg_reset()
-        elif opt in ("-a", "--active"):
+        elif opt in ("-t", "--target"):
             print("active target slot: %d" % flocklab.tg_get_selected())
         elif opt in ("-s", "--select", "--sel"):
             try:
@@ -94,6 +92,16 @@ def main(argv):
                     print("Failed to set target voltage.")
             except:
                 print("Invalid voltage")
+        elif opt in ("-m", "--mux"):
+            if arg.lower() in ("e", "en", "enable", "1", "on"):
+                flocklab.tg_mux_en(True)
+            else:
+                flocklab.tg_mux_en(False)
+        elif opt in ("-a", "--act", "--actuation"):
+            if arg.lower() in ("e", "en", "enable", "1", "on"):
+                flocklab.tg_act_en(True)
+            else:
+                flocklab.tg_act_en(False)
         elif opt in ("-h", "--help"):
             usage()
             sys.exit(flocklab.SUCCESS)
